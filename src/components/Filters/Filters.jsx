38358e4e0c9acba55503692afe564e30f1d0c128
IrynaRybalka <129180240+IrynaRybalka@users.noreply.github.com>
2023-11-05 14:49:12 +0200

    task 1

---
+++ src/components/Filters/Filters.jsx
@@ -0,0 +1,13 @@
+import { Input } from './Filters.styled';
+
+export const Filters = ({ onChangeFilter }) => {
+  return (
+    <div>
+      Find number by name:
+      <Input
+        type="text"
+        onChange={evt => onChangeFilter(evt.target.value)}
+      ></Input>
+    </div>
+  );
+};
