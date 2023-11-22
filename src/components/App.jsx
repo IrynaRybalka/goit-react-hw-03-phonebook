e5c2f553533be906025749992486cb5d46caff4d
IrynaRybalka <129180240+IrynaRybalka@users.noreply.github.com>
2023-11-22 18:40:51 +0200

    updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,10 +15,23 @@ export class App extends Component {
     ],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
   addContact = newContact => {
-    const isContact = this.state.contacts.some(contact =>
-      contact.name.toLowerCase()
-        === newContact.name.toLowerCase()
+    const isContact = this.state.contacts.some(
+      contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
     );
     if (isContact) {
       alert(`${newContact.name} alredy in contacts`);
@@ -49,19 +62,7 @@ export class App extends Component {
     }));
     console.log(this.deleteContact);
   };
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
   render() {
     return (
       <Wrapper>
